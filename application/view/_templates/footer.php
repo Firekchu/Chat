ebfe41fec623c71755d4184676ae3466cdcb16cc
Lennard de Roij <lennardderoij@hotmail.com>
2016-05-19 09:02:21 +0200

    V0.01
    Test to working together

---
+++ application/view/_templates/footer.php
@@ -0,0 +1,13 @@
+    <!-- jQuery, loaded in the recommended protocol-less way -->
+    <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	
+	 <!-- CSS -->
+    <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
+
+    <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
+    <div>
+        <p>By Diego Jannsen and Lennard de Roij</p>
+    </div>
+</body>
+</html>
